5dd4f5083d80daac3ab9365c1c53ed9240dda629
Luis <hellshell2025@gmail.com>
2026-02-13 11:45:08 +0800

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://vujngwincyvyxzhitgsn.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1am5nd2luY3l2eXh6aGl0Z3NuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0Njk5ODgsImV4cCI6MjA4NjA0NTk4OH0.0Q-M_KTmeTXX-8gN-8JRJORIQNspMso-_TMkQH0if_g'
+// Import environment variables using Vite's import.meta.env object
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
